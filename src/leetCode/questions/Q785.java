ce27ffa8366d78bc6a170aaee06e63b59e2d8f1b
sarvesh Parab <sarveshsparab@gmail.com>
2018-09-03 17:34:59 -0700

    bipartitite solution 1
    doesnt work for multi independendt connect graphs

--- src/leetCode/questions/Q785.java
+++ src/leetCode/questions/Q785.java
@@ -7,27 +7,62 @@ import java.util.Queue;
 
 public class Q785 {
 
-    private int[] nodeLevel = new int[101];
-    private boolean[] visitedNodes = new boolean[101];
+    private static int[] nodeLevel = new int[101];
+    private static boolean[] visitedNodes = new boolean[101];
     private static Queue<Integer> q = new LinkedList<>();
-    private Map<Integer,Integer> unvisitedEdges = new HashMap<>();
+    private static Map<Integer,Integer> unvisitedEdges = new HashMap<>();
 
-    public boolean isBipartite(int[][] graph) {
-        int currentLevel = 0 ;
-        nodeLevel[0] = currentLevel;
+    public static void main(String args[]){
+        /*System.out.println(isBipartite(new int[][]{
+                {1,3}, {0,2}, {1,3}, {0,2}
+        }));*/
+
+        /*System.out.println(isBipartite(new int[][]{
+                {1,2,3}, {0,2}, {0,1,3}, {0,2}
+        }));*/
+
+        System.out.println(checkBipartite(new int[][]{
+                {}
+        }));
+
+    }
+
+    public static boolean isBipartite(int[][] graph) {
+        for(int i=0;i<visitedNodes.length;i++)
+            if(!visitedNodes[i])
+                if(!checkBipartite(graph))
+                    return false;
+
+        return true;
+    }
+
+    public static boolean checkBipartite(int[][] graph) {
 
         visitedNodes[0] = true;
         q.add(0);
+        nodeLevel[0] = 1;
 
         while(!q.isEmpty()) {
             int currentNode = q.poll();
+            int[] currentNodeNeighbors = graph[currentNode];
 
+            for(int child : currentNodeNeighbors)
+                nodeLevel[child]=nodeLevel[child]==0?nodeLevel[currentNode]+1:nodeLevel[child];
 
-
+            for(int newNode : currentNodeNeighbors){
+                if(!visitedNodes[newNode]){
+                    visitedNodes[newNode]=true;
+                    q.add(newNode);
+                }else
+                    unvisitedEdges.put(currentNode,newNode);
+            }
         }
 
+        for(Map.Entry<Integer, Integer> entry : unvisitedEdges.entrySet())
+            if(Math.abs(nodeLevel[entry.getKey()]-nodeLevel[entry.getValue()])==0)
+                return false;
 
-        return false;
+        return true;
     }
 
 }
